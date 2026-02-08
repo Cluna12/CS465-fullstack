b342d2d91f4939c89088f70f59f5844cd8a402f6
Caleb Luna <calebluna2000@gmail.com>
2026-02-08 16:14:43 -0600

    Module 5 completed

--- travlr/app.js
+++ travlr/app.js
@@ -7,7 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index'); 
 var handlebars = require('hbs'); 
+
+//bring in database
+require('./app_api/models/db');
+
+
+
+
 
 var app = express();
 
@@ -21,16 +29,12 @@ handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
 
-
-
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/api', apiRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
